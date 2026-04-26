f52e99eab0b15d16d938f6e8d667d5801ca6eb00
Dempsey Palacio Tascon <dempsey.2001@hotmail.co.uk>
2026-04-26 14:59:24 +0100

    add comments to Navbar component for better code clarity

--- FE/chores/src/components/Navbar.tsx
+++ FE/chores/src/components/Navbar.tsx
@@ -15,10 +15,12 @@ export default function Navbar({ user }: NavbarProps) {
 
   return (
     <nav className="w-11/12 h-16 p-2 max-w-full rounded-3xl shadow-lg fixed top-5 bg-[#e59d50]/70 flex z-20 backdrop-blur-lg">
+      {/* Left Page Options */}
       <div className="h-full flex items-center grow gap-4">
         <NavLink path="/home" label="Home" />
         {user.role == "admin" && <NavLink path="/stats" label="Stats" />}
       </div>
+      {/* Right User Profile */}
       <div className="relative">
         <div
           id="userProfile"
